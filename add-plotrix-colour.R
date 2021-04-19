74941fa920c4b0de766f80a3e64e90f73cff3ec0
Roland Tanglao <roland@rolandtanglao.com`>
2021-04-18 21:02:47 -0700

    use "" instead of "NA" in the CSV file

--- add-plotrix-colour.R
+++ add-plotrix-colour.R
@@ -5,5 +5,5 @@ df_with_plotrix_colour <- df %>%
   rowwise() %>% 
   mutate(synth_plotrixcolour = 
            if_else(synth_75sqisvalid == 0, "", color.id(synth_75imaveragecolour)[1]))
-# Note, "' gets converted to "NA" by write_csv()
-write_csv(df_with_plotrix_colour, "/Users/roland/Documents/GIT/files_too_big_for_github_rt-flickr-sqlite-csv/2020-and-2019-roland-flickr-filename-imagemagick-imavgcolour-plotrixavgcolour-metadata.csv")
+# Note, "' gets converted to "NA" by write_csv() by default
+write_csv(df_with_plotrix_colour, "/Users/roland/Documents/GIT/files_too_big_for_github_rt-flickr-sqlite-csv/2020-and-2019-roland-flickr-filename-imavgcolour-plotrixavgcolour-metadata.csv", na="")
