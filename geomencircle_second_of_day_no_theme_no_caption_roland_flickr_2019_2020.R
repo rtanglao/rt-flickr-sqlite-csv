d021dc5236b6e1c9d675c0f10c7200e1405080a7
Roland Tanglao <roland@rolandtanglao.com`>
2021-09-20 16:49:14 -0700

    geom_encircle second of day scales = 'free'

---
+++ geomencircle_second_of_day_no_theme_no_caption_roland_flickr_2019_2020.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+library(ggalt)
+roland_f_2019_2020_narrow_dataset <- read_csv("https://media.githubusercontent.com/media/rtanglao/rt-flickr-sqlite-csv/main/LARGE_CSV_FILES/pacific-yyyy-mm-dd-2020-2019-roland-flickr-datetaken-synth_75sqisvalid-synth_plotrixcolour_unixtimedt.csv")
+second_of_day_roland_f_2019_2020_narrow_dataset <- 
+  roland_f_2019_2020_narrow_dataset %>% 
+  rowwise() %>% 
+  mutate(second_of_day = 
+           hour(as_datetime(unixtime_dt, tz="America/Vancouver")) * 3600 +
+           minute(as_datetime(unixtime_dt, tz="America/Vancouver")) * 60 +
+           second(as_datetime(unixtime_dt, tz="America/Vancouver")))
+second_of_day_roland_flickr_20192020_average_colour_plot_colors_factor <- ggplot(second_of_day_roland_f_2019_2020_narrow_dataset,  aes(second_of_day,synth_plotrixcolour))
+geomencircle_roland_flickr_20192020_average_colour_plot_colors_plot <- 
+  second_of_day_roland_flickr_20192020_average_colour_plot_colors_factor +
+  geom_encircle(aes(group=synth_plotrixcolour,
+                    colour=I(synth_plotrixcolour),
+                    fill=I(synth_plotrixcolour))) + 
+  geom_point(aes(colour=I(synth_plotrixcolour))) +
+  facet_wrap(~ pacific_ymd, scales = "free") +
+  theme_void() +
+  theme(
+    strip.background = element_blank(),
+    strip.text.x = element_blank()
+  )  
