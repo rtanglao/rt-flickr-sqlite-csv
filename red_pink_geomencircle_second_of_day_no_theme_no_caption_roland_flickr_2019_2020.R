597552ea4e83f241e2974c7bbd4832eea52a773e
Roland Tanglao <roland@rolandtanglao.com`>
2021-10-17 10:54:24 -0700

    add draw over

--- red_pink_geomencircle_second_of_day_no_theme_no_caption_roland_flickr_2019_2020.R
+++ red_pink_geomencircle_second_of_day_no_theme_no_caption_roland_flickr_2019_2020.R
@@ -8,8 +8,8 @@ red_pink_second_of_day_roland_f_2019_2020_narrow_dataset <-
   red_pink_roland_f_2019_2020_narrow_dataset %>% 
   rowwise() %>% 
   mutate(second_of_day = 
-           hour(as_datetime(unixtime_dt, tz="America/Vancouver")) * 3600 +
-           minute(as_datetime(unixtime_dt, tz="America/Vancouver")) * 60 +
+           hour(as_datetime(unixtime_dt, tz = "America/Vancouver")) * 3600 +
+           minute(as_datetime(unixtime_dt, tz = "America/Vancouver")) * 60 +
            second(as_datetime(unixtime_dt, tz="America/Vancouver")))
 red_pink_second_of_day_roland_flickr_20192020_average_colour_plot_colours_factor <- ggplot(red_pink_second_of_day_roland_f_2019_2020_narrow_dataset,  aes(second_of_day,synth_plotrixcolour))
 red_pink_geomencircle_roland_flickr_20192020_average_colour_plot_colours_plot <- 
